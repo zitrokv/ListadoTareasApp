5252c6bc5d4c974a5ed6ec1719342655b01ddef7
zitrokv <victor.vialasc@gmail.com>
2024-07-03 13:59:31 +0200

    lista de Tareas para CRUD, revisar binding en Render por el comportamiento del checkbox, esta corregido

--- app/src/main/java/com/example/listadotareasapp/MainActivity.kt
+++ app/src/main/java/com/example/listadotareasapp/MainActivity.kt
@@ -3,30 +3,42 @@ package com.example.listadotareasapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listadotareasapp.data.Tarea
 import com.example.listadotareasapp.data.TareaDAO
 import com.example.listadotareasapp.databinding.ActivityMainBinding
+import com.example.listadotareasapp.utils.TareaAdapter
 import java.lang.reflect.Array.getBoolean
 import java.lang.reflect.Array.getInt
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+    private lateinit var adapter: TareaAdapter
+    private lateinit var taskDAO: TareaDAO
+    lateinit var tareaList: List<Tarea>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val taskDAO = TareaDAO(this)
+        taskDAO = TareaDAO(this)
 
-        /*var task = Tarea(-1, "Comprar leche", false)
-        taskDAO.insert(task)
-
-        Log.i("DATABASE", task.toString())
+        /*taskDAO.insert(Tarea(-1, "Comprar leche", false))
+        taskDAO.insert(Tarea(-1, "Ir al banco", false))
+        taskDAO.insert(Tarea(-1, "Entrenar", false))
+        taskDAO.insert(Tarea(-1, "Raspar pescado", false))
+        taskDAO.insert(Tarea(-1, "Hacer aplicaciones", false))
+        taskDAO.insert(Tarea(-1, "Abono del tren", false))
+        taskDAO.insert(Tarea(-1, "Organizar entierro", false))
+*/
+       /* Log.i("DATABASE", task.toString())
 
         task.realizada = true
 
@@ -44,15 +56,55 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("DATABASE", taskList.toString())*/
 
-        val task = taskDAO.findAll()
+        adapter = TareaAdapter(
+            emptyList(), {
+            Toast.makeText(
+                this,
+                "Click en ${tareaList[it].nombre}",
+                Toast.LENGTH_LONG
+            ).show()},
+            { taskDAO.delete(tareaList[it])
+                Toast.makeText(
+                    this,
+                    "Click en ${tareaList[it].nombre}",
+                    Toast.LENGTH_LONG
+                ).show()
+                loadData()
+            },
 
-        //binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+            {  tareaList[it].realizada = !tareaList[it].realizada
+                taskDAO.update(tareaList[it])
+              loadData()
+            }
+        )
 
 
+        /*adapter = TareaAdapter(tareaList) {
+            Toast.makeText(
+                this,
+                "Click en ${tareaList[it].nombre}",
+                Toast.LENGTH_LONG
+            ).show()
+        }*/
+
+        binding.recyclerView.adapter = adapter
+        //binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+        //binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
 
+        //loadData()
+    }
 
+    override fun onResume() {
+        super.onResume()
 
+        loadData()
+    }
+
+    private fun loadData() {
+        tareaList = taskDAO.findAll()
+
+        adapter.updateData(tareaList)
     }
 }
--- app/src/main/java/com/example/listadotareasapp/utils/TareaAdapter.kt
+++ app/src/main/java/com/example/listadotareasapp/utils/TareaAdapter.kt
@@ -1,15 +1,21 @@
 package com.example.listadotareasapp.utils
 
 import android.graphics.Color
+import android.support.v4.os.IResultReceiver._Parcel
 import android.text.SpannableString
 import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.listadotareasapp.data.Tarea
 import com.example.listadotareasapp.databinding.ItemTareaBinding
+import com.google.android.material.checkbox.MaterialCheckBox.OnCheckedStateChangedListener
 
-class TareaAdapter(private var dataSet: List<Tarea> = emptyList(), private val onItemClickListener: (Int) -> Unit) : RecyclerView.Adapter<TareaViewHolder>(){
+class TareaAdapter(private var dataSet: List<Tarea> = emptyList(),
+                   private val onItemClickListener: (Int) -> Unit,
+                   private val onDeleteClickListener: (Int) -> Unit,
+                   private val onCheckBoxChangedListener: (Int) -> Unit) : RecyclerView.Adapter<TareaViewHolder>(){
 
     private var highlightText: String? = null
 
@@ -24,17 +30,26 @@ class TareaAdapter(private var dataSet: List<Tarea> = emptyList(), private val o
     override fun onBindViewHolder(holder: TareaViewHolder, position: Int) {
         holder.render(dataSet[position])
         holder.itemView.setOnClickListener{
-            onItemClickListener(position)
-            //onItemClickListener(holder.adapterPosition) // es la posicion más real, se calcula dinamicamente!!
+            //onItemClickListener(position)
+            onItemClickListener(holder.adapterPosition) // es la posicion más real, se calcula dinamicamente!!
+        }
+        holder.binding.deleteImageButton.setOnClickListener(){
+            onDeleteClickListener(holder.adapterPosition)
         }
 
-        if (highlightText != null) {
-            holder.highlight(highlightText!!)
+        holder.binding.realizadoCheckBox.setOnCheckedChangeListener {chk , _ ->
+        if (chk.isPressed)
+            onCheckBoxChangedListener(holder.adapterPosition)
+
         }
 
-        //aqui declaramos el evento de click onItemClickListener
-        //holder.itemView.setOnClickListener {onItemClickListener(position)}
 
+
+    }
+
+    fun updateData(dataSet: List<Tarea> ) {
+        this.dataSet = dataSet
+        notifyDataSetChanged()
     }
 
     /*fun updateData(dataSet: List<SuperheroResponse>, highlight: String? ){
@@ -46,10 +61,11 @@ class TareaAdapter(private var dataSet: List<Tarea> = emptyList(), private val o
 
 }
 
-class TareaViewHolder(private val binding: ItemTareaBinding) : RecyclerView.ViewHolder(binding.root)
+class TareaViewHolder(val binding: ItemTareaBinding) : RecyclerView.ViewHolder(binding.root)
 {
     fun render(tarea: Tarea){
         binding.nameTextView.text = tarea.nombre
+        binding.realizadoCheckBox.isChecked = tarea.realizada
     }
 
     // Subraya el texto que coincide con la busqueda
