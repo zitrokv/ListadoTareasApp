eee990d104c3856bd4dada203d0bc5f164b72bca
zitrokv <victor.vialasc@gmail.com>
2024-07-05 14:14:52 +0200

    alta y modificacion (pendiente) de las tareas, creado con alertDialog para mostrar flotante

--- app/src/main/java/com/example/listadotareasapp/activities/DetalleActivity.kt
+++ app/src/main/java/com/example/listadotareasapp/activities/DetalleActivity.kt
@@ -1,15 +1,25 @@
 package com.example.listadotareasapp.activities
 
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
 import com.example.listadotareasapp.R
 import com.example.listadotareasapp.data.Tarea
 import com.example.listadotareasapp.data.TareaDAO
 import com.example.listadotareasapp.databinding.ActivityDetalleBinding
+import java.util.Calendar
 
-class DetalleActivity : AppCompatActivity() {
+/*class DetalleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetalleBinding
 
     private lateinit var taskDAO: TareaDAO
@@ -29,4 +39,173 @@ class DetalleActivity : AppCompatActivity() {
             finish()
         }
     }
+}*/
+
+
+class DetalleActivity : FragmentActivity(),
+NoticeDialogFragment.NoticeDialogListener {
+
+    fun showNoticeDialog() {
+        Toast.makeText(this,"aqui llega",Toast.LENGTH_SHORT).show()
+        // Create an instance of the dialog fragment and show it.
+        val dialog = NoticeDialogFragment()
+        dialog.show(supportFragmentManager, "NoticeDialogFragment")
+    }
+
+    override fun onCreatePanelView(featureId: Int): View? {
+
+        return super.onCreatePanelView(featureId)
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following
+    // methods defined by the NoticeDialogFragment.NoticeDialogListener
+    // interface.
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        // User taps the dialog's positive button.
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        // User taps the dialog's negative button.
+    }
+
 }
+
+
+
+
+
+
+class NoticeDialogFragment : DialogFragment() {
+    // Use this instance of the interface to deliver action events.
+    internal lateinit var listener: NoticeDialogListener
+
+    // The activity that creates an instance of this dialog fragment must
+    // implement this interface to receive event callbacks. Each method passes
+    // the DialogFragment in case the host needs to query it.
+    interface NoticeDialogListener {
+        fun onDialogPositiveClick(dialog: DialogFragment)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the
+    // NoticeDialogListener.
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // Verify that the host activity implements the callback interface.
+        try {
+            // Instantiate the NoticeDialogListener so you can send events to
+            // the host.
+            listener = context as NoticeDialogListener
+        } catch (e: ClassCastException) {
+            // The activity doesn't implement the interface. Throw exception.
+            throw ClassCastException((context.toString() +
+                    " must implement NoticeDialogListener"))
+        }
+    }
+
+
+    private lateinit var binding: ActivityDetalleBinding
+
+    private lateinit var taskDAO: TareaDAO
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetalleBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+
+        taskDAO = TareaDAO(binding.nameEditText.context)
+
+        binding.saveButton.setOnClickListener {
+
+            /* binding.deadLineDatePicker.setOnDateChangedListener{
+               val calendar = Calendar.getInstance()
+                calendar.set(binding.deadLineDatePicker.year,
+                    binding.deadLineDatePicker.month,
+                    binding.deadLineDatePicker.dayOfMonth)
+            }*/
+            val taskName = binding.nameEditText.text.toString()
+            val task = Tarea(-1, taskName, false)
+            taskDAO.insert(task)
+            Toast.makeText(binding.nameEditText.context, "Tarea guardad correctamente", Toast.LENGTH_SHORT).show()
+            //finish()
+        }
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout to use as a dialog or embedded fragment.
+        return inflater.inflate(R.layout.activity_detalle, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        /*val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return dialog*/
+
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction.
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+
+            /*builder.setMessage("Start game")
+                .setPositiveButton("Start") { dialog, id ->
+                    // START THE GAME!
+                }
+                .setNegativeButton("Cancel") { dialog, id ->
+                    // User cancelled the dialog.
+                }
+                */
+
+            // Create the AlertDialog object and return it.
+            //builder.create()
+
+
+
+            binding.saveButton.setOnClickListener {
+                val taskName = binding.nameEditText.text.toString()
+                val task = Tarea(-1, taskName, false)
+                taskDAO.insert(task)
+                Toast.makeText( binding.saveButton.context, "Tarea guardad correctamente", Toast.LENGTH_SHORT).show()
+                //finish()
+            }
+
+
+            /*************/
+            // Get the layout inflater.
+            //val inflater: LayoutInflater
+
+            //inflater.inflate(R.layout.activity_detalle, binding.root)
+
+            // Inflate and set the layout for the dialog.
+            // Pass null as the parent view because it's going in the dialog
+            // layout.
+            builder.setView(inflater.inflate(R.layout.activity_detalle, binding.root))
+                // Add action buttons.
+                .setPositiveButton("Sing in", //R.string.signin,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // Sign in the user.
+                    })
+                .setNegativeButton("Cancel", //R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        getDialog()?.cancel()
+
+                    })
+            builder.create()
+
+        } ?: throw IllegalStateException("Activity cannot be null")
+
+
+    }
+
+
+}
+
+
--- app/src/main/java/com/example/listadotareasapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/listadotareasapp/activities/MainActivity.kt
@@ -1,23 +1,43 @@
 package com.example.listadotareasapp.activities
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.CalendarView
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.DialogTitle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.listadotareasapp.R
 import com.example.listadotareasapp.data.Tarea
 import com.example.listadotareasapp.data.TareaDAO
+import com.example.listadotareasapp.databinding.ActivityDetalleBinding
 import com.example.listadotareasapp.databinding.ActivityMainBinding
 import com.example.listadotareasapp.utils.TareaAdapter
+import java.util.Calendar
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+
+
 
     lateinit var binding: ActivityMainBinding
     private lateinit var adapter: TareaAdapter
     private lateinit var taskDAO: TareaDAO
     lateinit var tareaList: List<Tarea>
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,19 +75,19 @@ class MainActivity : AppCompatActivity() {
 
         adapter = TareaAdapter(
             emptyList(), {
-                Toast.makeText(
+                /*Toast.makeText(
                     this,
                     "Click en ${tareaList[it].nombre}",
                     Toast.LENGTH_LONG
-                ).show()
+                ).show()*/
             },
             {
                 taskDAO.delete(tareaList[it])
-                Toast.makeText(
+                /*Toast.makeText(
                     this,
                     "Click en ${tareaList[it].nombre}",
                     Toast.LENGTH_LONG
-                ).show()
+                ).show()*/
                 loadData()
             },
 
@@ -77,7 +97,9 @@ class MainActivity : AppCompatActivity() {
                 loadData()
             },
             {
-                mostrarDialog()
+                //showDialog()
+                mostrarDialog("Modificar tarea", tareaList[it].nombre)
+                loadData()
             }
         )
 
@@ -96,12 +118,9 @@ class MainActivity : AppCompatActivity() {
         //binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         binding.addTaskButton.setOnClickListener {
-            val intent = Intent(this, DetalleActivity::class.java)
-            startActivity(intent)
+            mostrarDialog("Añadir nueva tarea", "",true)
         }
 
-        //mostrarDialog()
-        //loadData()
     }
 
     override fun onResume() {
@@ -116,38 +135,121 @@ class MainActivity : AppCompatActivity() {
         adapter.updateData(tareaList)
     }
 
-    fun mostrarDialog() {
+     fun mostrarDialog(title: String, message: String, esNuevaTarea:Boolean = false) {
     val builder = AlertDialog.Builder(this)
-    builder.setTitle("Androidly Alert")
-    builder.setMessage("We have a message")
+         builder.setTitle(title)
+
+         //builder.setMessage("Fecha a rellenar...")
 //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+        val dialogBinding = ActivityDetalleBinding.inflate(layoutInflater)
 
-    builder.setPositiveButton(android.R.string.yes)
+        builder.setView(dialogBinding.root)
+
+
+
+         //botones comunes OK Cancel
+         builder.setPositiveButton(android.R.string.yes)
+         { dialog, which ->
+
+
+             /*Toast.makeText(
+                 this,
+                 android.R.string.yes, Toast.LENGTH_SHORT
+             ).show()*/
+         }
+
+         builder.setNegativeButton(android.R.string.no)
+         { dialog, which ->
+             /*Toast.makeText(
+                 this,
+                 android.R.string.no, Toast.LENGTH_SHORT
+             ).show()*/
+         }
+
+    /*builder.setNeutralButton("Maybe")
     {
         dialog, which ->
         Toast.makeText(
-            applicationContext,
-            android.R.string.yes, Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    builder.setNegativeButton(android.R.string.no)
-    {
-        dialog, which ->
-        Toast.makeText(
-            applicationContext,
-            android.R.string.no, Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    builder.setNeutralButton("Maybe")
-    {
-        dialog, which ->
-        Toast.makeText(
-            applicationContext,
+            this,
             "Maybe", Toast.LENGTH_SHORT
         ).show()
-    }
-    builder.show()
+    }*/
+
+    //mostrar en pantalla
+    val dialog = builder.create()
+
+
+         //si es modificación muestra los botones
+         if (!esNuevaTarea) {
+             dialogBinding.nameEditText.visibility = View.GONE
+             builder.setMessage(message)
+
+         } else
+         {
+
+             dialogBinding.saveButton.setOnClickListener {
+                 val taskName = dialogBinding.nameEditText.text.toString()
+                 val task = Tarea(-1, taskName, false)
+                 taskDAO.insert(task)
+                 Toast.makeText(this, "Tarea guardad correctamente", Toast.LENGTH_SHORT).show()
+                 loadData()
+                 dialog.dismiss()
+                 //finish()
+             }
+
+             /*
+             val c = Calendar.getInstance()
+             val year = c.get(Calendar.YEAR)
+             val month = c.get(Calendar.MONTH)
+             val day = c.get(Calendar.DAY_OF_MONTH)
+             val picker = DatePickerDialog(this as Context, , year, month, day)
+*/
+
+         }
+
+         dialog.show()
 }
+
+    class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit) :
+        DialogFragment(), DatePickerDialog.OnDateSetListener {
+        override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val picker = DatePickerDialog(activity as Context, this, year, month, day)
+            return picker
+        }
+    }
+
+    fun showDialog() {
+        val fragmentManager = supportFragmentManager
+        val newFragment = DetalleActivity()
+        //val newFragment = fragmentManager.
+        //if (isLargeLayout) {
+        if (true){
+            // The device is using a large layout, so show the fragment as a
+            // dialog.
+            //newFragment.show(fragmentManager, "dialog")
+
+            newFragment.showNoticeDialog()
+        } else {
+            // The device is smaller, so show the fragment fullscreen.
+            val transaction = fragmentManager.beginTransaction()
+            // For a polished look, specify a transition animation.
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            // To make it fullscreen, use the 'content' root view as the container
+            // for the fragment, which is always the root view for the activity.
+
+            transaction
+                //.add(binding.root, newFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
 }
